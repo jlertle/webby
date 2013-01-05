d69d652f4188a6fe8849677481fe9350ac31ac44
Christopher Jackson <himself@cj-jackson.com>
2013-01-05 10:40:29 +0000

    Added Error403, Error404 and Error 500 method! to Web!

--- view.go
+++ view.go
@@ -8,6 +8,10 @@ var Error403 = func(w *Web) {
 	w.Print("<h1>403 Forbidded</h1>")
 }
 
+func (web *Web) Error403() {
+	Error403(web)
+}
+
 // Overridable Error404 Function
 //
 // Note:  Overriding is useful for custom 404 page
@@ -16,12 +20,20 @@ var Error404 = func(w *Web) {
 	w.Print("<h1>404 Not Found</h1>")
 }
 
+func (web *Web) Error404() {
+	Error404(web)
+}
+
 // Overridable Error500 Function
 //
 // Note:  Overriding is useful for custom 500 page
 var Error500 = func(w *Web) {
 	w.Status = 500
 	w.Print("<h1>500 Internal Server Error</h1>")
+}
+
+func (web *Web) Error500() {
+	Error500(web)
 }
 
 // Default Index View
