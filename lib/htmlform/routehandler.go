eafd19530f42697d5773b174c02df46c6cff98c4
Christopher Jackson <himself@cj-jackson.com>
2013-01-06 21:26:38 +0000

    Added RouteHnalder to 'lib/htmlform'!

---
+++ lib/htmlform/routehandler.go
@@ -0,0 +1,69 @@
+package htmlform
+
+import (
+	"github.com/CJ-Jackson/webby"
+)
+
+type RouteInterface interface {
+	Init(*webby.Web) bool
+	FetchForm(*webby.Web) *Form
+	Get(*webby.Web, *Form)
+	PostPass(*webby.Web)
+	PostFail(*webby.Web)
+}
+
+type RouteHandler struct {
+	RouteInterface
+}
+
+func (ro RouteHandler) View(w *webby.Web) {
+	truth := ro.Init(w)
+
+	if w.CutOut() {
+		return
+	}
+
+	if !truth {
+		w.Error403()
+		return
+	}
+
+	var form *Form
+
+	switch t := w.Session.(type) {
+	case *Form:
+		form = t
+		w.DestroySession()
+	default:
+		form = ro.FetchForm(w)
+	}
+
+	if w.Req.URL.RawQuery != "" {
+		if form.IsValid(w) {
+			goto pass
+		} else {
+			goto get
+		}
+	}
+
+	if w.Req.Method == "POST" {
+		goto post
+	}
+
+get:
+	ro.Get(w, form)
+	return
+
+post:
+	if !form.IsValid(w) {
+		goto fail
+	}
+
+pass:
+	ro.PostPass(w)
+	return
+
+fail:
+	w.SetSession(form)
+	ro.PostFail(w)
+}
