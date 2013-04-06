1f75bb427a705134574600ddf41235994aa32138
Christopher Jackson <himself@cj-jackson.com>
2013-04-06 12:00:56 +0100

    Fully commented time.go

--- time.go
+++ time.go
@@ -4,29 +4,32 @@ import (
 	"time"
 )
 
-var (
-	// Default Time Location
-	DefaultTimeLoc *time.Location
-	// Default Time Format
-	DefaultTimeFormat string = "Monday, _2 January 2006, 15:04"
-)
+// Default Time Location
+var DefaultTimeLoc *time.Location
 
+// Default Time Format
+var DefaultTimeFormat string = "Monday, _2 January 2006, 15:04"
+
+// Set Timezone on global level
 func SetTimeZone(zone string) {
 	var err error
 	DefaultTimeLoc, err = time.LoadLocation(zone)
 	Check(err)
 }
 
+// Set Timezone on user request level
 func (w *Web) SetTimeZone(zone string) {
 	var err error
 	w.TimeLoc, err = time.LoadLocation(zone)
 	w.Check(err)
 }
 
+// Get Current Time
 func CurTime() time.Time {
 	return time.Now()
 }
 
+// Get Current Time
 func (w *Web) CurTime() time.Time {
 	return CurTime()
 }
