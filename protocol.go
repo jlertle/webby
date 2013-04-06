81ecc5ac8d26fec850f56b4f9ccbb2efffd565a0
Christopher Jackson <himself@cj-jackson.com>
2013-04-06 11:51:34 +0100

    Fully commented protocol.go

--- protocol.go
+++ protocol.go
@@ -37,33 +37,40 @@ type PipeProtocol struct {
 	pr Protocol
 }
 
+// PipeProtocol constructor
 func NewProtocol() PipeProtocol {
 	return PipeProtocol{Protocol{}}
 }
 
+// Get Protocol
 func (pi PipeProtocol) Get() Protocol {
 	return pi.pr
 }
 
+// Set Http
 func (pi PipeProtocol) Http(http RouteHandler) PipeProtocol {
 	pi.pr.HTTP = http
 	return pi
 }
 
+// Set Https
 func (pi PipeProtocol) Https(https RouteHandler) PipeProtocol {
 	pi.pr.HTTPS = https
 	return pi
 }
 
+// Set All
 func (pi PipeProtocol) All(all RouteHandler) PipeProtocol {
 	pi.pr.ALL = all
 	return pi
 }
 
+// Alais of All
 func (pi PipeProtocol) Any(any RouteHandler) PipeProtocol {
 	return pi.All(any)
 }
 
+// Alais of All
 func (pi PipeProtocol) Fallback(fallback RouteHandler) PipeProtocol {
 	return pi.All(fallback)
 }
