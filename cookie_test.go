096f843ab67449b536682524527a1a1422f7f119
Christopher Jackson <himself@cj-jackson.com>
2013-04-17 19:46:49 +0100

    Updated Cookie example!

--- cookie_test.go
+++ cookie_test.go
@@ -7,8 +7,13 @@ func ExampleWeb_Cookie(w *Web) {
 	// Setting a Cookie, Secure and Http Only.
 	w.Cookie("Example").Value("Example").Month().HttpOnly().Secure().SaveRes()
 
-	// Get Cookie from User Request!
-	cookie, _ := w.Cookie("Exaample").Get()
+	// Get Cookie from User Request, just omit 'Value' simple as that! Should return *http.Cookie and error
+	cookie, err := w.Cookie("Example").Get()
+
+	if err != nil {
+		// If the cookie does not exist just set a new cookie and get it.
+		cookie, _ = w.Cookie("Example").Value("Example").Month().SaveRes().Get()
+	}
 
 	// Delete a Cookie
 	w.Cookie(cookie.Name).Delete()
