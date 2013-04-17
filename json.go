8655e97dcfb2c1ce4b1499c6b6a477b7a31376ab
Christopher Jackson <himself@cj-jackson.com>
2013-04-17 19:34:49 +0100

    Improvement to Json and Xml!

--- json.go
+++ json.go
@@ -2,6 +2,7 @@ package webby
 
 import (
 	"encoding/json"
+	"io"
 )
 
 type Json struct {
@@ -12,7 +13,37 @@ func (w *Web) Json() Json {
 	return Json{w}
 }
 
-// Output in JSON
+// Shortcut to encoding/json.Marshal
+func (j Json) Marshal(v interface{}) ([]byte, error) {
+	return json.Marshal(v)
+}
+
+// Shortcut to encoding/json.MarshalIndent
+func (j Json) MarshalIndent(v interface{}, prefix, indent string) ([]byte, error) {
+	return json.MarshalIndent(v, prefix, indent)
+}
+
+// Shortcut to encoding/json.Unmarshal
+func (j Json) Unmarshal(data []byte, v interface{}) error {
+	return json.Unmarshal(data, v)
+}
+
+// Shortcut to encoding/json.NewDecoder
+func (j Json) NewDecoder(r io.Reader) *json.Decoder {
+	return json.NewDecoder(r)
+}
+
+// Shortcut to encoding/json.NewEncoder
+func (j Json) NewEncoder(w io.Writer) *json.Encoder {
+	return json.NewEncoder(w)
+}
+
+// Send Json output to client.
 func (j Json) Send(v interface{}) {
-	json.NewEncoder(j.w).Encode(v)
+	j.NewEncoder(j.w).Encode(v)
+}
+
+// Decode Request Body
+func (j Json) DecodeReqBody(v interface{}) error {
+	return j.NewDecoder(j.w.Req.Body).Decode(v)
 }
--- xml.go
+++ xml.go
@@ -2,6 +2,7 @@ package webby
 
 import (
 	"encoding/xml"
+	"io"
 )
 
 type Xml struct {
@@ -12,7 +13,37 @@ func (w *Web) Xml() Xml {
 	return Xml{w}
 }
 
+// Shortcut to encoding/xml.Marshal
+func (x Xml) Marshal(v interface{}) ([]byte, error) {
+	return xml.Marshal(v)
+}
+
+// Shortcut to encoding/xml.MarshalIndent
+func (x Xml) MarshalIndent(v interface{}, prefix, indent string) ([]byte, error) {
+	return xml.MarshalIndent(v, prefix, indent)
+}
+
+// Shortcut to encoding/xml.Unmarshal
+func (x Xml) Unmarshal(data []byte, v interface{}) error {
+	return xml.Unmarshal(data, v)
+}
+
+// Shortcut to encoding/xml.NewDecoder
+func (x Xml) NewDecoder(r io.Reader) *xml.Decoder {
+	return xml.NewDecoder(r)
+}
+
+// Shortcut to encoding/xml.NewEncoder
+func (x Xml) NewEncoder(w io.Writer) *xml.Encoder {
+	return xml.NewEncoder(w)
+}
+
 // Output in XML
 func (x Xml) Send(v interface{}) {
 	xml.NewEncoder(x.w).Encode(v)
+}
+
+// Decode Request Body
+func (x Xml) DecodeReqBody(v interface{}) error {
+	return x.NewDecoder(x.w.Req.Body).Decode(v)
 }
