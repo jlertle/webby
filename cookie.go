05e6e6c9b5dae807caab781d6b55111b0c525fbb
Christopher Jackson <himself@cj-jackson.com>
2013-04-24 15:23:52 +0100

    Renamed PipeCookie to Cookie!

--- cookie.go
+++ cookie.go
@@ -21,44 +21,44 @@ func init() {
 }
 
 // Chainable version of 'net/http.Cookie'
-type PipeCookie struct {
+type Cookie struct {
 	w *Web
 	c *http.Cookie
 }
 
 // New Cookie
-func NewCookie(w *Web, name string) PipeCookie {
-	return PipeCookie{
+func NewCookie(w *Web, name string) Cookie {
+	return Cookie{
 		w: w,
 		c: &http.Cookie{Name: name},
 	}
 }
 
 // Cookie
-func (w *Web) Cookie(name string) PipeCookie {
+func (w *Web) Cookie(name string) Cookie {
 	return NewCookie(w, name)
 }
 
 // Set Value
-func (c PipeCookie) Value(value string) PipeCookie {
+func (c Cookie) Value(value string) Cookie {
 	c.c.Value = value
 	return c
 }
 
 // Set Path
-func (c PipeCookie) Path(path string) PipeCookie {
+func (c Cookie) Path(path string) Cookie {
 	c.c.Path = path
 	return c
 }
 
 // Set Domain
-func (c PipeCookie) Domain(domain string) PipeCookie {
+func (c Cookie) Domain(domain string) Cookie {
 	c.c.Domain = domain
 	return c
 }
 
 // Set Expiry Time of Cookie.
-func (c PipeCookie) Expires(expires time.Time) PipeCookie {
+func (c Cookie) Expires(expires time.Time) Cookie {
 	c.c.Expires = expires
 	return c
 }
@@ -66,25 +66,25 @@ func (c PipeCookie) Expires(expires time.Time) PipeCookie {
 // MaxAge=0 means no 'Max-Age' attribute specified.
 // MaxAge<0 means delete cookie now, equivalently 'Max-Age: 0'
 // MaxAge>0 means Max-Age attribute present and given in seconds
-func (c PipeCookie) MaxAge(maxage int) PipeCookie {
+func (c Cookie) MaxAge(maxage int) Cookie {
 	c.c.MaxAge = maxage
 	return c
 }
 
 // Make Cookie Secure
-func (c PipeCookie) Secure() PipeCookie {
+func (c Cookie) Secure() Cookie {
 	c.c.Secure = true
 	return c
 }
 
 // Make Cookie Http Only
-func (c PipeCookie) HttpOnly() PipeCookie {
+func (c Cookie) HttpOnly() Cookie {
 	c.c.HttpOnly = true
 	return c
 }
 
 // Get *http.Cookie, if Value is not set it will try to get the Cookie from the User Request!
-func (c PipeCookie) Get() (*http.Cookie, error) {
+func (c Cookie) Get() (*http.Cookie, error) {
 	if c.c.Value != "" {
 		return c.c, nil
 	}
@@ -92,18 +92,18 @@ func (c PipeCookie) Get() (*http.Cookie, error) {
 }
 
 // Delete Cookie
-func (c PipeCookie) Delete() PipeCookie {
+func (c Cookie) Delete() Cookie {
 	return c.Value("Delete-Me").MaxAge(-1).SaveRes()
 }
 
 // Save (Set) Cookie to Response
-func (c PipeCookie) SaveRes() PipeCookie {
+func (c Cookie) SaveRes() Cookie {
 	http.SetCookie(c.w, c.pre(c.c))
 	return c
 }
 
 // Prepare Cookie
-func (c PipeCookie) pre(cookie *http.Cookie) *http.Cookie {
+func (c Cookie) pre(cookie *http.Cookie) *http.Cookie {
 	var num int
 	w := c.w
 
@@ -152,62 +152,62 @@ release_cookie:
 
 // Save (Add) Cookie to Request, It won't send anything out to the client.
 // But it is a useful feature for CSRF protection for example!.
-func (c PipeCookie) SaveReq() PipeCookie {
+func (c Cookie) SaveReq() Cookie {
 	c.w.Req.AddCookie(c.c)
 	return c
 }
 
 // Set to Expire after an hour
-func (c PipeCookie) Hour() PipeCookie {
+func (c Cookie) Hour() Cookie {
 	return c.Expires(time.Now().Add(1 * time.Hour))
 }
 
 // Set to Expire after 6 Hours
-func (c PipeCookie) Hour6() PipeCookie {
+func (c Cookie) Hour6() Cookie {
 	return c.Expires(time.Now().Add(6 * time.Hour))
 }
 
 // Set to Expire after 12 Hours
-func (c PipeCookie) Hour12() PipeCookie {
+func (c Cookie) Hour12() Cookie {
 	return c.Expires(time.Now().Add(12 * time.Hour))
 }
 
 // Set to Expire after 1 Day
-func (c PipeCookie) Day() PipeCookie {
+func (c Cookie) Day() Cookie {
 	return c.Expires(time.Now().AddDate(0, 0, 1))
 }
 
 // Set to Expire after 1 Week
-func (c PipeCookie) Week() PipeCookie {
+func (c Cookie) Week() Cookie {
 	return c.Expires(time.Now().AddDate(0, 0, 1*7))
 }
 
 // Set to Expire after 2 Week
-func (c PipeCookie) Week2() PipeCookie {
+func (c Cookie) Week2() Cookie {
 	return c.Expires(time.Now().AddDate(0, 0, 2*7))
 }
 
 // Set to Expire after 1 Month
-func (c PipeCookie) Month() PipeCookie {
+func (c Cookie) Month() Cookie {
 	return c.Expires(time.Now().AddDate(0, 1, 0))
 }
 
 // Set to Expire after 3 Month
-func (c PipeCookie) Month3() PipeCookie {
+func (c Cookie) Month3() Cookie {
 	return c.Expires(time.Now().AddDate(0, 3, 0))
 }
 
 // Set to Expire after 6 Month
-func (c PipeCookie) Month6() PipeCookie {
+func (c Cookie) Month6() Cookie {
 	return c.Expires(time.Now().AddDate(0, 6, 0))
 }
 
 // Set to Expire after 9 Month
-func (c PipeCookie) Month9() PipeCookie {
+func (c Cookie) Month9() Cookie {
 	return c.Expires(time.Now().AddDate(0, 9, 0))
 }
 
 // Set to Expire after 1 Year
-func (c PipeCookie) Year() PipeCookie {
+func (c Cookie) Year() Cookie {
 	return c.Expires(time.Now().AddDate(1, 0, 0))
 }
